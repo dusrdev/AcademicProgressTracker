9fe502a49ec8e9f49d554d3d9bdd609fca8c4139
David Shnayder <dusrdev@gmail.com>
2021-01-17 22:42:05 +0200

    Added base models

---
+++ AcademicProgressTrackerBLL/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AcademicProgressTrackerBLL.Models
+{
+    public class Course
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public int Value { get; set; }
+        public double Grade { get; set; }
+        public List<Course> Requirements { get; set; } 
+    }
+}
---
+++ AcademicProgressTrackerBLL/Models/Degree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademicProgressTrackerBLL.Models
+{
+    public class Degree
+    {
+        public string Title { get; set; }
+        public double Grade { get; set; }
+        public int RequiredPoints { get; set; }
+        public List<Course> PassedCourses { get; set; }
+        public List<Course> FutureCourses { get; set; }
+    }
+}
